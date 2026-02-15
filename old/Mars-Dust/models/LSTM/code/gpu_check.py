881902a08aa97877c3347335024605613feff82c
jhuss433-sketch <jhuss433@gmail.com>
2026-02-15 09:09:57 -0500

    cleaning branch

--- old/Mars-Dust/models/LSTM/code/gpu_check.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-print("TF Version:", tf.__version__)
-print("Built with CUDA:", tf.test.is_built_with_cuda())
-print("GPUs Available:", tf.config.list_physical_devices('GPU'))
